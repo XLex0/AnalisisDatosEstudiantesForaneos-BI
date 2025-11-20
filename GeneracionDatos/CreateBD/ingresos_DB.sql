f9db78929fbda73d94f403048a6702cc07f0ff8e
XLex0 <alexander.redes17@gmail.com>
2025-11-20 02:43:43 -0500

    Datos creados cargados

--- GeneracionDatos/CreateBD/ingresos_DB.sql
+++ GeneracionDatos/CreateBD/ingresos_DB.sql
@@ -13,8 +13,8 @@ CREATE TABLE tipo_universidad (
 
 CREATE TABLE estudiante (
     id_estudiante INT PRIMARY KEY,
-    nombre VARCHAR(100),
-    apellido VARCHAR(100),
+    nombre VARCHAR(200),
+    apellido VARCHAR(200),
     es_becado BOOLEAN,
     id_tipo_universidad INT,
     FOREIGN KEY (id_tipo_universidad) REFERENCES tipo_universidad(id_tipo_universidad)
@@ -28,3 +28,18 @@ CREATE TABLE ingreso (
     FOREIGN KEY (id_tipo_ingreso) REFERENCES tipo_ingreso(id_tipo_ingreso),
     FOREIGN KEY (id_estudiante) REFERENCES estudiante(id_estudiante)
 );
+
+
+INSERT INTO ingresos.tipo_ingreso (nombre)
+VALUES
+('Apoyo familiar'),
+('Trabajo'),
+('Crédito educativo'),
+('Otro'),
+('Beca');
+
+INSERT INTO ingresos.tipo_universidad (nombre)
+VALUES
+('Pública'),
+('Privada'),
+('NA');
