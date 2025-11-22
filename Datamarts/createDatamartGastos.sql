c47a562eb05120796a969c09551a27a738f8d2f0
XLex0 <alexander.redes17@gmail.com>
2025-11-22 02:50:01 -0500

    Cargar datos a OLAP finished

--- Datamarts/createDatamartGastos.sql
+++ Datamarts/createDatamartGastos.sql
@@ -1,65 +1,60 @@
+CREATE DATABASE IF NOT EXISTS dm_gastos;
 
-CREATE DATABASE IF NOT EXISTS mart_gastos;
 
--- ---------- Dimensiones ----------
-CREATE TABLE IF NOT EXISTS mart_gastos.dim_periodo
-(
-  id_periodo UInt32,                                  -- PK
-  semestre LowCardinality(String),
-  anio UInt16,
-  etiqueta_periodo LowCardinality(String)
-)
-ENGINE = MergeTree
-ORDER BY id_periodo;
-
-CREATE TABLE IF NOT EXISTS mart_gastos.dim_universidad
-(
-  id_universidad UInt32,                              -- PK
-  nombre_universidad LowCardinality(String),
-  carrera LowCardinality(String)
-)
-ENGINE = MergeTree
-ORDER BY id_universidad;
-
-CREATE TABLE IF NOT EXISTS mart_gastos.dim_origen
-(
-  id_origen UInt32,                                   -- PK
-  ciudad LowCardinality(String),
-  provincia LowCardinality(String)
-)
-ENGINE = MergeTree
-ORDER BY id_origen;
-
-CREATE TABLE IF NOT EXISTS mart_gastos.dim_estudiante
-(
-  id_estudiante UInt32,                               -- PK
-  genero LowCardinality(String),
-  edad UInt8,
-  modalidad LowCardinality(String),                   -- Presencial/Híbrida/Virtual
-  es_becado UInt8                                     -- 0/1
+CREATE TABLE dm_gastos.dim_estudiante (
+    id_estudiante String,   
+    edad Int32,
+    genero String,
+    modalidad String,
+    becado UInt8            -- 0/1
 )
 ENGINE = MergeTree
 ORDER BY id_estudiante;
 
-CREATE TABLE IF NOT EXISTS mart_gastos.dim_categoria_gasto
-(
-  id_categoria_gasto UInt32,                          -- PK
-  categoria LowCardinality(String)                    -- alimentación, vivienda, etc.
+
+CREATE TABLE dm_gastos.dim_periodo (
+    id_periodo UInt32,
+    semestre Int32,
+    anio Int32,
+    periodo String          -- etiqueta_periodo
+)
+ENGINE = MergeTree
+ORDER BY id_periodo;
+
+
+CREATE TABLE dm_gastos.dim_universidad (
+    id_universidad UInt32,
+    universidad String,
+    carrera String
+)
+ENGINE = MergeTree
+ORDER BY id_universidad;
+
+
+CREATE TABLE dm_gastos.dim_categoria_gasto (
+    id_categoria_gasto UInt32,
+    categoria String
 )
 ENGINE = MergeTree
 ORDER BY id_categoria_gasto;
 
--- ---------- Hechos ----------
-CREATE TABLE IF NOT EXISTS mart_gastos.hechos_gasto
-(
-  monto_gasto Float32,
 
-  -- Claves (FK lógicas)
-  id_estudiante       UInt32,
-  id_periodo          UInt32,
-  id_universidad      UInt32,
-  id_categoria_gasto  UInt32,
-  id_origen           UInt32
+CREATE TABLE dm_gastos.dim_origen (
+    id_origen UInt32,
+    ciudad String,
+    provincia String
 )
 ENGINE = MergeTree
-ORDER BY (id_estudiante, id_periodo, id_universidad, id_categoria_gasto, id_origen);
+ORDER BY id_origen;
+
+
+CREATE TABLE dm_gastos.fact_gasto (
+    id_estudiante String,
+    id_periodo UInt32,
+    id_universidad UInt32,
+    id_categoria_gasto UInt32,
+    id_origen UInt32,
+    monto_gasto Float64
+)
+ENGINE = MergeTree
+ORDER BY (id_estudiante, id_periodo, id_categoria_gasto);
